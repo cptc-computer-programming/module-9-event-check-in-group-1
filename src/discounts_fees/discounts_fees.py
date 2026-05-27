b625893ea138ee13f6b830e52e2d6cf2348e03fb
Cody Brown <cody.thecodeman71@gmail.com>
2026-05-27 14:43:05 -0700

    added push fee

--- src/discounts_fees/discounts_fees.py
+++ src/discounts_fees/discounts_fees.py
@@ -15,16 +15,35 @@ This module is your team's workspace for discount and service-fee calculations.
 # 5. Get instructor approval for both contracts.
 #
 # Data contract template for Function 1:
-# Function name: services_fee
+# Function name: service_fee
 # Purpose: To determine the total fees for the the reservation.
 # Parameters: total_guests_in_reservation, is_online, subtotal
-# Returns: service_fee
+# Returns: returned_service_fee
 # Possible return values: 0 (0$) or maybe 5 (5$)
 # Assumptions:
 # Example call:
 # Example result:
 # Who might use this function? The team for Final Check-In Decision
 #
+
+
+def service_fee(total_guests_in_reservation, is_online, subtotal):
+
+    IS_ONLINE_FEE = 5
+
+    service_fee_percent = 0
+    returned_service_fee = 0
+
+    if is_online:
+       service_fee_percent = IS_ONLINE_FEE
+       returned_service_fee = subtotal * service_fee_percent
+    else:
+        returned_service_fee = subtotal * service_fee_percent
+
+
+    return(returned_service_fee)
+
+
 # Data contract template for Function 2:
 # Function name: discount_amount
 # Purpose: To determine the total discounts for the reservation.
@@ -36,4 +55,9 @@ This module is your team's workspace for discount and service-fee calculations.
 # Example result:
 # Who might use this function? The team for Final Check-In Decision
 #
-# TODO: After approval, write your two function definitions below.
+
+
+
+
+
+# TODO: After approval, write your two function definitions under each funtion comment above here.
